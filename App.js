59065d072310ea01ee1fdfa53c5b63e9aa5725b4
Yusuf Ensar Cebeci <96048976+yusufensarcebeci@users.noreply.github.com>
2025-11-05 23:44:31 +0300

    update project structure and initial setup

--- App.js
+++ App.js
@@ -4,23 +4,43 @@ export class App {
         this.width = width;
         this.height = height;
         this.app = null;
+        this.onResize = null;
     }
 
     async createApp() {
-        this.app = new PIXI.Application();
-        await this.app.init({ width: this.width, height: this.height })
-        this.createCanvas()
+
+        try {
+            this.app = new PIXI.Application();
+            await this.app.init({
+                width: this.width, height: this.height,
+                backgroundColor: 'FFC070', antialias: true,
+                resolution: window.devicePixelRatio || 1
+            })
+            this.createCanvas()
+            console.log("%c[App]", "color: #7fff00", "PixiJS initialized successfully");
+
+            // Scene container (ileride sahne yöneticisi için temel)
+            // this.scene = new PIXI.Container();
+            // this.app.stage.addChild(this.scene);
+        } catch (e) {
+            console.error("PixiJS initialization failed:", error);
+        }
+
     }
 
-    createCanvas(){
+    createCanvas() {
         document.body.appendChild(this.app.canvas);
     }
 
     resizeCanvas(width, height) {
-        this.app.renderer.resize(width , height);
+        if (!this.app) return;
+        this.app.renderer.resize(width, height);
+        if (typeof this.onResize === "function") {
+            this.onResize(width, height);
+        }
+
+        console.log(`[App] Canvas resized to: ${width}x${height}`);
         // this.app.stage.scale.set(1);
+        // Bu sayede ileride app.onResize = (w,h)=> { sceneManager.resize(w,h) } gibi bağlantılar kurabilirsin.
     }
-
-    
 }
-
--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import { App } from './App.js';
+import { GameManager } from './src/core/GameManager.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded', async () => {
 
     let GAME_WIDTH = window.innerWidth;
     let GAME_HEIGHT = window.innerHeight;
+
     const app = new App(GAME_WIDTH, GAME_HEIGHT);
-    app.createApp();
-    // app.initializeGame();
+    const gameManager = new GameManager;
+
+    app.createApp().then(()=>{
+        gameManager.initializeGame();
+    });
+
     window.addEventListener('resize', () => {
         GAME_WIDTH = window.innerWidth;
         GAME_HEIGHT = window.innerHeight;   
         app.resizeCanvas(GAME_WIDTH, GAME_HEIGHT)
     });
-        console.log("gdsgasd")
 
+    console.log("%c[Main]", "color: cyan", "Game initialized and ready");
 });
